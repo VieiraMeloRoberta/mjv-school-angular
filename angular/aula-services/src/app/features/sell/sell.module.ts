8e7fe409ba6a47e020d58bb7dd306ed296a16b22
Roberta Vieira de Melo <roberta.vieirademelo@yahoo.com.br>
2023-04-14 11:03:17 -0300

    aula services

---
+++ angular/aula-services/src/app/features/sell/sell.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductsComponent } from './components/products/products.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { SellPageComponent } from './pages/sell-page/sell-page.component';
+import { DetailsPageComponent } from './pages/details-page/details-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    ProductsComponent,
+    SellPageComponent,
+    DetailsPageComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule
+  ],
+  exports: [
+    ProductsComponent,
+    SellPageComponent
+  ]
+})
+export class SellModule { }
